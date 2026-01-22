814c56f92f7e6c9456013e6269018b95ed43aeb6
UCL-LatentSpace <ucwares@ucl.ac.uk>
2026-01-22 19:21:17 +0000

    Added map and modal components

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './tw-styles.css'
+import TitleBar from './components/TitleBar'
+import MapDisplay from './components/MapDisplay' 
+import PlaqueModal from './components/PlaqueModal'
 
 function App() {
-
   const [count, setCount] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  function increment() {
-    setCount(count + 1)
-  }
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={increment}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+return (
+  <div className="mx-auto max-w-screen-xl bg-cyan-500 min-h-screen">
+    <TitleBar title="🤡 RM OpenPlaques Recommended Reading" />
+    <MapDisplay longitude={-9.165209} latitude={38.717856} zoom={16} />
+    {isModalOpen ? <PlaqueModal /> : null}
+  </div>
+)
 }
 
 export default App
